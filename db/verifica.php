c5d6cd1d77df4fb0661aadb3c472483a695b04e2
Guilherme Facury <68427192+guifacury@users.noreply.github.com>
2023-11-24 08:52:35 -0300

    Update verifica.php

--- db/verifica.php
+++ db/verifica.php
@@ -15,22 +15,4 @@ try {
     die("Could not connect to the database $dbname :" . $e->getMessage());
 }
 
-
-
-// $host = 'localhost';
-// $dbname = 'u714680025_abarateira';
-// $username = 'u714680025_abarateira';
-// $password = '+7bE~;fOVdU';
-// global $pdo;
-
-// try {
-//     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-// } catch (PDOException $e) {
-//     die("Could not connect to the database $dbname :" . $e->getMessage());
-// }
-
-
-
-
-
 ?>
